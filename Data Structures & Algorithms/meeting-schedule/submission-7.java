56743d436280953b9e54046f868126f94d266f83
Yuvraj Singh Rathore <yuvisinghrathore678678@gmail.com>
2026-08-27 21:40:49 +0530

    Add: meeting-schedule - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule/submission-7.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i).start < intervals.get(i - 1).end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
